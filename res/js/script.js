017882470683f194ed60c73b544c5797500bc2ab
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2020-12-29 22:52:43 -0600

    Script Update
    Fixed the select cloning jquery after changing class names.
    
    Added id for some optimize html elements for easier accessibility in js.

--- res/js/script.js
+++ res/js/script.js
@@ -49,7 +49,7 @@ const main = async () => {
             state.sortedData[i].id !== 24115 &&
             state.sortedData[i].id !== 24117
         ) {
-            $('.optimizer__equippedGear__selection').append(
+            $('.optimize__optimizer__characterInfo__equippedGear__select').append(
                 $('<option>', {
                     id: state.sortedData[i].id,
                     value: state.sortedData[i].name,
@@ -57,7 +57,7 @@ const main = async () => {
                 })
             )
 
-            $('.optimizer__collectionGear__selection').append(
+            $('.optimize__optimizer__characterInfo__collectionGear__select').append(
                 $('<option>', {
                     id: state.sortedData[i].id,
                     value: state.sortedData[i].name,
@@ -68,13 +68,34 @@ const main = async () => {
     }
 
     // Clone Equipped Gear Select Element
-    for (let i = 0; i < 6; i++) {
-        $('.optimizer__equippedGear__table__tableRow:last-child').clone().appendTo('.optimizer__equippedGear__table__tableBody')
-    }
-    // Clone Collection Gear Select Element
-    for (let i = 0; i < 19; i++) {
-        $('.optimizer__collectionGear__table__tableRow:last-child').clone().appendTo('.optimizer__collectionGear__table__tableBody')
-    }
+    $('#equippedGear1').clone().prop('id', 'equippedGear2').prependTo('#equippegGearField2')
+    $('#equippedGear1').clone().prop('id', 'equippedGear3').prependTo('#equippegGearField3')
+    $('#equippedGear1').clone().prop('id', 'equippedGear4').prependTo('#equippegGearField4')
+    $('#equippedGear1').clone().prop('id', 'equippedGear5').prependTo('#equippegGearField5')
+    $('#equippedGear1').clone().prop('id', 'equippedGear6').prependTo('#equippegGearField6')
+    $('#equippedGear1').clone().prop('id', 'equippedGear7').prependTo('#equippegGearField7')
+
+    $('#equippedGear1').clone().prop('id', 'collectionGear1').prependTo('#collectionGearField1')
+    $('#equippedGear1').clone().prop('id', 'collectionGear2').prependTo('#collectionGearField2')
+    $('#equippedGear1').clone().prop('id', 'collectionGear3').prependTo('#collectionGearField3')
+    $('#equippedGear1').clone().prop('id', 'collectionGear4').prependTo('#collectionGearField4')
+    $('#equippedGear1').clone().prop('id', 'collectionGear5').prependTo('#collectionGearField5')
+    $('#equippedGear1').clone().prop('id', 'collectionGear6').prependTo('#collectionGearField6')
+    $('#equippedGear1').clone().prop('id', 'collectionGear7').prependTo('#collectionGearField7')
+    $('#equippedGear1').clone().prop('id', 'collectionGear8').prependTo('#collectionGearField8')
+    $('#equippedGear1').clone().prop('id', 'collectionGear9').prependTo('#collectionGearField9')
+    $('#equippedGear1').clone().prop('id', 'collectionGear10').prependTo('#collectionGearField10')
+    $('#equippedGear1').clone().prop('id', 'collectionGear11').prependTo('#collectionGearField11')
+    $('#equippedGear1').clone().prop('id', 'collectionGear12').prependTo('#collectionGearField12')
+    $('#equippedGear1').clone().prop('id', 'collectionGear13').prependTo('#collectionGearField13')
+    $('#equippedGear1').clone().prop('id', 'collectionGear14').prependTo('#collectionGearField14')
+    $('#equippedGear1').clone().prop('id', 'collectionGear15').prependTo('#collectionGearField15')
+    $('#equippedGear1').clone().prop('id', 'collectionGear16').prependTo('#collectionGearField16')
+    $('#equippedGear1').clone().prop('id', 'collectionGear17').prependTo('#collectionGearField17')
+    $('#equippedGear1').clone().prop('id', 'collectionGear18').prependTo('#collectionGearField18')
+    $('#equippedGear1').clone().prop('id', 'collectionGear19').prependTo('#collectionGearField19')
+    $('#equippedGear1').clone().prop('id', 'collectionGear20').prependTo('#collectionGearField20')
+
 
     //Add listener for Updates on any input on change
     updateInputsEventlisteners()
