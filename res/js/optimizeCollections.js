726465e3105874f4483b047b3b873384b94c1287
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2021-01-04 23:08:56 -0600

    Optimize Function Update
    Created the function that calculates the best positions for each gear piece.
    
    Added heading to optimize html.
    
    Fixed typo in updateInputs.js
    Fixed type in otimize.html

--- res/js/optimizeCollections.js
+++ res/js/optimizeCollections.js
@@ -7,7 +7,66 @@ const OptimizeCollections = () => {
 }
 
 const optiomizeCollectionMaxPower = () => {
-    console.log(state.collectionGearFinalStats)
+    let changed = true
+
+    while(changed) {
+        changed = false
+        for(let i = 1; i <= 20; i++) {
+            for(let l = 1; l <= 20; l++) {
+                if(l !== i) {
+                    if(swapGear(`slot${i}`, `slot${l}`)) {
+                        changed = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+const swapGear = (currSlot, otherSlot) => {
+    const currSlotPrecentage = state.collectionPercentages[currSlot]
+    const currSlotStats = state.collectionGearFinalStats[currSlot]
+    const otherSlotPrecentage = state.collectionPercentages[otherSlot]
+    const otherSlotStats = state.collectionGearFinalStats[otherSlot]
+
+    const currGearDefaultActiveStat = (currSlotPrecentage.type === 'attack' ? currSlotStats.attack :
+        currSlotPrecentage.type === 'defense' ? currSlotStats.defense :
+        currSlotPrecentage.type === 'magic' ? currSlotStats.magic :
+        currSlotPrecentage.type === 'health' && currSlotStats.health
+    ) * currSlotPrecentage.value / 100
+
+    const currGearOtherActiveStat = (otherSlotPrecentage.type === 'attack' ? currSlotStats.attack :
+        otherSlotPrecentage.type === 'defense' ? currSlotStats.defense :
+        otherSlotPrecentage.type === 'magic' ? currSlotStats.magic :
+        otherSlotPrecentage.type === 'health' && currSlotStats.health
+    ) * otherSlotPrecentage.value / 100
+
+    const otherGearDefaultActiveStat = (otherSlotPrecentage.type === 'attack' ? otherSlotStats.attack :
+        otherSlotPrecentage.type === 'defense' ? otherSlotStats.defense :
+        otherSlotPrecentage.type === 'magic' ? otherSlotStats.magic :
+        otherSlotPrecentage.type === 'health' && otherSlotStats.health
+    ) * otherSlotPrecentage.value / 100
+
+    const otherGearCurrActiveStat = (currSlotPrecentage.type === 'attack' ? otherSlotStats.attack :
+        currSlotPrecentage.type === 'defense' ? otherSlotStats.defense :
+        currSlotPrecentage.type === 'magic' ? otherSlotStats.magic :
+        currSlotPrecentage.type === 'health' && otherSlotStats.health
+    ) * currSlotPrecentage.value / 100
+
+    const defaultActiveStatSum = currGearDefaultActiveStat + otherGearDefaultActiveStat
+    const otherActiveStatSum = currGearOtherActiveStat + otherGearCurrActiveStat
+
+    if (defaultActiveStatSum < otherActiveStatSum) {
+        console.log('swap')
+        const temp = currSlotStats
+        state.collectionGearFinalStats[currSlot] = otherSlotStats
+        state.collectionGearFinalStats[otherSlot] = temp
+
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 export const optimizeCollectionsListener = () => {
